827f652c3469c4553cca1ff7598339412b1a08b4
Casey Reed <casey.w.reed@gmail.com>
2017-02-02 20:03:26 -0600

    Updated MVP

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Wes Bos's YouTube Ad Skip
 
-Inspired by [this tweet](https://twitter.com/wesbos/status/827268492936114176)
+Inspired by [this tweet](https://twitter.com/wesbos/status/827268492936114176), this extension will continously run some code in the background of all YouTube pages and wait for an ad to show up. When it does, it'll skip it for you!
+
+No bulky ad blockers. Just about 30 lines of JavaScript.
--- scripts.js
+++ scripts.js
@@ -1,7 +1,35 @@
-const timer = setInterval(function () {
-    let video = document.querySelector('video')
-    if (video) {
-        document.querySelector('video').currentTime = document.querySelector('video').duration
-        clearInterval(timer)
+//Every 500ms, check whether the browser's URL has changed or not
+let urlChangeHandler = window.setInterval(checkUrlChange, 500)
+let oldUrl= ""
+
+function checkUrlChange(){
+    //Continuously grab the window's URL
+    let newUrl = document.URL
+    if(newUrl !== oldUrl){
+        // If it's different from the previous URL, check for an ad on the page
+        checkForAd()
+        .then(function (check) {
+            if (check) {
+                // If there was an ad playing on the page, skip it..
+                skipAd()
+                // Then replace the oldUrl with the new one and continu
+                oldUrl = newUrl
+            }
+        })
+        .catch(function (err) {
+            console.error(err)
+        })
     }
-}, 500);
+}
+
+function checkForAd () {
+    return new Promise(function (resolve, reject) {
+        let adCheck = document.querySelector('.videoAdUi')
+        resolve(adCheck)
+    })
+}
+
+// Shoutout to Wes Bos for this slick line of code...
+function skipAd () {
+    document.querySelector('video').currentTime = document.querySelector('video').duration
+}
